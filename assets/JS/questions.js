878679d264a9f1718aebf098ff39bfb7ebc5a536
James Antley <jimmy.antley@gmail.com>
2021-04-22 17:50:46 -0400

    fixed my questions.js file

--- assets/JS/questions.js
+++ assets/JS/questions.js
@@ -4,44 +4,28 @@ const quizQuestions = [
         choices: ["Strings", "Booleans", "Alerts", "Numbers"],
         correctAnswer: "Alerts"
     },
-    {
-        question: "Commonly used Data Types do not include:",
-        choices: ["asdasd", "fghfgh", "234254", "123"],
-        correctAnswer: "123"
-    },
+  
     {
         question: "The condition in an if/else statement is enclosed within _______.",
-        a: "Quotes",
-        b: "Curly brackets",
-        c: "Parenthesis",
-        d: "Square brackets",
+        choices: ["Quotes", "Curly brackets", "Parenthesis", "Square brackets"],
         correctAnswer: "c"
     },
 
     {
         question: "Arrays in JavaScript can be used to store _____.",
-        a: "Numbers and strings",
-        b: "Other arrays",
-        c: "Booleans",
-        d: "All of the above",
+        choices: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
         correctAnswer: "d"
     },
 
     {
         question: "String Values must be enclosed within _____ when being assigned to variables.",
-        a: "Commas",
-        b: "Curly brackets",
-        c: "Quotes",
-        d: "Parenthesis",
+        choices: ["Commas", "Curly brackets", "Quotes", "Parenthesis"],
         correctAnswer: "c"
     },
 
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        a: "JavaScript",
-        b: "Terminal/bash",
-        c: "For loops",
-        d: "Console.log",
+        choices: ["JavaScript", "Terminal/bash", "For loops", "Console.log"],
         correctAnswer: "d"
     }
 ]

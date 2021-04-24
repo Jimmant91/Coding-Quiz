3999e8f6d418d3e3a3ded609fb30a014d0636907
James Antley <jimmy.antley@gmail.com>
2021-04-24 15:44:34 -0400

    accidentally delted all my files and had to restore

---
+++ assets/js/timer.js
@@ -0,0 +1,22 @@
+let timeLeft;
+let timerInterval;
+
+function startCountdown() {
+    timeLeft = 75;
+    // Use the `setInterval()` method to call a function to be executed every 1000 milliseconds
+    timeInterval = setInterval(function () {
+        
+        // Set the `textContent` of `timerEl` to show the remaining seconds
+        timerEl.textContent = "Timer: " + timeLeft;
+        // Decrement `timeLeft` by 1
+        timeLeft--;
+
+        if (timeLeft < 1) {
+            // Once `timeLeft` gets to 0, set `timerEl` to 'Time is up!'
+            alert.textContent = 'Time is up!';
+            // Use `clearInterval()` to stop the timer
+            clearInterval(timeInterval);
+        }
+
+    }, 1000);
+}

d99f287e808244396ad7cbe6e21f48fcc66874e9
James Antley <79285782+Jimmant91@users.noreply.github.com>
2021-04-24 14:53:04 -0400

    Delete assets directory

--- assets/JS/DOMselectors.js
+++
@@ -1,7 +0,0 @@
-const startBox = document.getElementById('startBox')
-const quizBox = document.getElementById('quizBox')
-const userInitialsBox = document.getElementById('userInitialsBox')
-const highscoresBox = document.getElementById('highscoresBox')
-
-const timerEl = document.getElementById('timer');
-const startButton = document.getElementById('startBtn');
